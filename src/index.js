828a44619766590bbecb4c14601415e6b3de7b79
Praneeth <praneethayla@gmail.com>
2023-12-11 15:45:38 +0530

    style.css into multiple files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import { navBar } from "./modules/navBar.js"
 import { homePage } from "./modules/home.js"
-import "./style.css";
 import { menuPage } from "./modules/menu.js";
+import "./style/style.css"
+import "./style/navBarStyle.css"
+import "./style/homeStyle.css"
+import "./style/menuStyle.css"
 
 const container = document.getElementById('container')
 
 
 navBar()
-// homePage()
-menuPage()
+homePage()
+// menuPage()
